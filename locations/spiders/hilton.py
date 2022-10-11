87d2d3a72664dd1dd59853d243fcc48a5c1f6cf8
Joe Dixon <mjoe999@gmail.com>
2022-10-11 15:48:21 +0100

    WIP

--- locations/spiders/hilton.py
+++ locations/spiders/hilton.py
@@ -1,30 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from locations.linked_data_parser import LinkedDataParser
+from locations.structured_data_spider import StructuredDataSpider
 
 
-def from_wikidata(name, code):
-    return {"brand": name, "brand_wikidata": code}
-
-
-class HiltonSpider(scrapy.spiders.SitemapSpider):
+class HiltonSpider(scrapy.spiders.SitemapSpider, StructuredDataSpider):
     name = "hilton"
-    sitemap_urls = [
-        "https://www.hilton.com/temporary/legacy-sitemap1.xml",
-        "https://www.hilton.com/temporary/legacy-sitemap2.xml",
-    ]
+    sitemap_urls = ["https://www.hilton.com/sitemap.xml"]
     download_delay = 0.2
-    HILTON_TRU = from_wikidata("Tru by Hilton", "Q24907770")
-    HILTON_WALDORF = from_wikidata("Waldorf Astoria", "Q3239392")
-    HILTON_GARDEN = from_wikidata("Hilton Garden Inn", "Q1162859")
-    HILTON_EMBASSY = from_wikidata("Embassy Suites", "Q5369524")
-    HILTON_HOMEWOOD = from_wikidata("Homewood Suites by Hilton", "Q5890701")
-    HILTON_HOME2 = from_wikidata("Home2 Suites by Hilton", "Q5887912")
-    HILTON_DOUBLETREE = from_wikidata("DoubleTree by Hilton", "Q2504643")
-    HILTON_CANOPY = from_wikidata("Canopy by Hilton", "Q30632909")
-    HILTON_CONRAD = from_wikidata("Conrad Hotels & Resorts", "Q855525")
-    HILTON_HAMPTON = from_wikidata("Hampton by Hilton", "Q5646230")
-    HILTON_HOTELS = from_wikidata("Hilton Hotels & Resorts", "Q598884")
+
+    HILTON_TRU = ["Tru by Hilton", "Q24907770"]
+    HILTON_WALDORF = ["Waldorf Astoria", "Q3239392"]
+    HILTON_GARDEN = ["Hilton Garden Inn", "Q1162859"]
+    HILTON_EMBASSY = ["Embassy Suites", "Q5369524"]
+    HILTON_HOMEWOOD = ["Homewood Suites by Hilton", "Q5890701"]
+    HILTON_HOME2 = ["Home2 Suites by Hilton", "Q5887912"]
+    HILTON_DOUBLETREE = ["DoubleTree by Hilton", "Q2504643"]
+    HILTON_CANOPY = ["Canopy by Hilton", "Q30632909"]
+    HILTON_CONRAD = ["Conrad Hotels & Resorts", "Q855525"]
+    HILTON_HAMPTON = ["Hampton by Hilton", "Q5646230"]
+    HILTON_HOTELS = ["Hilton Hotels & Resorts", "Q598884"]
     # Looks like internally Hilton assign each hotel a 7-alpha code, the last
     # two letters of which indicate the brand.
     my_brands = {
@@ -82,8 +76,7 @@ class HiltonSpider(scrapy.spiders.SitemapSpider):
             item = LinkedDataParser.parse(response, "Hotel")
             if item:
                 item["ref"] = response.url
-                item["brand"] = brand["brand"]
-                item["brand_wikidata"] = brand["brand_wikidata"]
+                item["brand"], item["brand_wikidata"] = brand
                 # The street address is set by Hilton to be the full address of the property.
                 # This can be fixed up rather well given that Hilton set the city field correctly.
                 street_address = item["street_address"]
